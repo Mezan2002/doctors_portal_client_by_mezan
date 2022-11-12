20a314c6e29b37c4286ec95bf38225ec0fe6e353
Mezan2002 <mezanurr009@gmail.com>
2022-11-12 16:00:46 +0600

    Setup Almost done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import { RouterProvider } from "react-router-dom";
 import "./App.css";
+import Routes from "./Routes/Routes/Routes";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <RouterProvider router={Routes}></RouterProvider>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className="footer p-10 bg-neutral text-neutral-content">
+        <div>
+          <span className="footer-title">Services</span>
+          <a className="link link-hover" href="/">
+            Branding
+          </a>
+          <a className="link link-hover" href="/">
+            Design
+          </a>
+          <a className="link link-hover" href="/">
+            Marketing
+          </a>
+          <a className="link link-hover" href="/">
+            Advertisement
+          </a>
+        </div>
+        <div>
+          <span className="footer-title">Company</span>
+          <a className="link link-hover" href="/">
+            About us
+          </a>
+          <a className="link link-hover" href="/">
+            Contact
+          </a>
+          <a className="link link-hover" href="/">
+            Jobs
+          </a>
+          <a className="link link-hover" href="/">
+            Press kit
+          </a>
+        </div>
+        <div>
+          <span className="footer-title">Legal</span>
+          <a className="link link-hover" href="/">
+            Terms of use
+          </a>
+          <a className="link link-hover" href="/">
+            Privacy policy
+          </a>
+          <a className="link link-hover" href="/">
+            Cookie policy
+          </a>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
