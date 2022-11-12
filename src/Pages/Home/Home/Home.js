e50109e48050cd8f55df83e268a80d4d66fa33e5
Mezan2002 <mezanurr009@gmail.com>
2022-11-12 22:49:21 +0600

    service card added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "../Banner/Banner";
 import InfoCard from "../InfoCard/InfoCard";
+import ServiceCards from "./ServiceCards/ServiceCards";
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
       <InfoCard></InfoCard>
+      <ServiceCards></ServiceCards>
     </div>
   );
 };
